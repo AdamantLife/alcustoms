1b6dad4c49153424a65dc05ccc86cca9081c97f6
RB3IT <43681720+RB3IT@users.noreply.github.com>
2021-02-08 15:10:12 -0500

    Added column width functions to excel module
    Added maxcolumnwidth and autowidth functions

--- alcustoms/excel/__init__.py
+++ alcustoms/excel/__init__.py
@@ -15,7 +15,44 @@ from alcustoms.excel.Tables import *
 lowerstrip = lambda key: key.lower().strip().replace(" ","_")
 
 
+def maxcolumnwidth(worksheet, column, start_row = None, end_row = None):
+    """ Determines the max length of the given column and returns it. This function ignores merged cells.
+    
+        worksheet should be a Worksheet instance.
+        column should be a number or a column letter.
+        start_row is the number of the row to start checking.
+        end_row is the number of the row to finish checking (inclusive).
+    """
+    if isinstance(column, str):
+        column = utils.column_index_from_string(column)
+
+    start_row = start_row or 1
+    end_row = end_row or worksheet.max_row
+    if end_row <= start_row:
+        raise ValueError("end_row should be greater or equal than start_row")
+
+    _max = 0
+    for row in range(start_row, end_row+1):
+        cell = worksheet.cell(row, column)
+        if cell.coordinate in worksheet.merged_cells:
+            continue
+        v = cell.value if cell.value is not None else ""
+        _max = max(_max, len(v))
+
+    return _max
 
 
 
+def autowidth(worksheet, column, start_row = None, end_row = None, padding = 0):
+    """ Sets the width of a given column to it's max width (via maxcolumnwidth).
+    
+        worksheet should be a Worksheet instance
+        column should be a number or a column letter.
+        start_row is the number of the row to start checking.
+        end_row is the number of the row to finish checking (inclusive).
+        padding should be an integer which will be added to the width.
+    """
 
+    width = maxcolumnwidth(worksheet = worksheet, column = column, start_row= start_row, end_row= end_row)
+    width += padding
+    worksheet.column_dimensions[column].width = width
