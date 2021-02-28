ca06e36465821a99306979b5fae51df0126a7889
AdamantLife <adamantlife@gmail.com>
2021-02-27 20:30:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="alcustoms-AdamantLife",
+    name="alcustoms",
     version="1.0.1",
     author="AdamantLife",
     author_email="",
